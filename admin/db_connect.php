b560c20fa366a877cd0856d3ac1afabfcff43842
vvuylsteker <vuylsteker@protonmail.ch>
2021-07-16 18:17:01 +0200

    force requetes preparés

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,10 +1,13 @@
 <?php
 
 function get_bdd(){
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-
-return new PDO("mysql:host=$servername;dbname=audreycoiffure", $username, $password);
-
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $bdd = new PDO("mysql:host=$servername;dbname=audreycoiffure", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $bdd;
 }
+
+?>
